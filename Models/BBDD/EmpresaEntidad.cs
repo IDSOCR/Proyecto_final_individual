599cb04f109f3f638b7abbe08a0284f803003432
Ibhar David Solera Quesada <isolera@outlook.com>
2024-04-21 15:47:56 -0600

    Agregar archivos de proyecto.

---
+++ Models/BBDD/EmpresaEntidad.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Models.BBDD
+{
+    public class EmpresaEntidad
+    {
+
+        [Range(0, int.MaxValue, ErrorMessage = "Debe seleccionar una empresa")]
+        [Display(Name ="Código")]
+        public int IDEmpresa { get; set; }
+
+        //
+
+        [Required(ErrorMessage = "Debe selecionar un tipo de empresa.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Debe selecionar un tipo de empresa.")]
+        [Display(Name = "Tipo Empresa")]
+        public int IDTipoEmpresa { get; set; }
+
+        //
+        [Required(ErrorMessage = "El Nombre de la empresa es obligatorio")]
+        [Display(Name = "Nombre Empresa")]
+        public string Empresa { get; set; } = string.Empty;
+
+        //
+        [Required(ErrorMessage = "La dirección de la empresa es obligatorio")]
+        [Display(Name = "Dirección")]
+        public string Direccion { get; set; } = string.Empty;
+
+        //
+        [Required(ErrorMessage = "El RUC de la empresa es obligatorio")]
+        [Display(Name = "RUC Empresa")]
+        public string RUC { get; set; } = string.Empty;
+
+        //
+        [Required(ErrorMessage = "Debe ingresar la fecha de creación")]
+        [Display(Name = "Fecha creación")]
+
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+
+        //
+        [Required(ErrorMessage = "Debe ingresar el presupuesto")]
+        [Display(Name = "Presupuesto")]
+        public decimal Presupuesto { get; set; }
+
+        //
+        
+        public bool Activo { get; set; } = true;
+        
+
+        //Propiedad de navegacion a TipoEmpresa
+        //
+
+
+        public TipoEmpresaEntidad? TipoEmpresa { get; set;}
+
+
+
+
+
+
+
+    }
+}
---
+++ Models/BBDD/TipoEmpresaEntidad.cs
@@ -0,0 +1,18 @@
+﻿namespace Models.BBDD
+{
+    public class TipoEmpresaEntidad
+    {
+
+        // son los misms campos de la tabla de la BBDD para el proyecto final
+        public int IDTipoEmpresa { get; set; }
+
+        public string TipoEmpresa { get; set; } = string.Empty;
+
+        public string Descripcion { get; set; } = string.Empty;
+
+        public string Sigla { get; set; } = string.Empty;
+
+
+        public bool Activo { get; set; } = true;
+    }
+}
